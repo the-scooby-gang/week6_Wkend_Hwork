eaf0bf1309c56387bbe4b5e96d0175c1cd2cc17b
Aimee Bunyan <aimeebunyan14091990@hotmail.co.uk>
2022-08-15 13:25:19 +0100

    updated the correct edited file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+
+  const age = 31;
+  const name = "Aimee";
+
+  return(
+      <div>
+        <h1 className = "title">Hello World!</h1>
+        <p> This is a paragraph of text</p>
+        <p>My name is {name} and I'm {age} years old</p>
+      </div>
   );
 }
 
